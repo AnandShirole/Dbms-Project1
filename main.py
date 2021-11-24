bdff59e70c423ad78778d43aebfcf04e36648281
AnandShirole <anand.21910161@viit.ac.in>
2021-11-24 09:52:48 +0530

    new changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -41,6 +41,35 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(1000))
 
 
+
+class Patients(db.Model):
+    pid=db.Column(db.Integer,primary_key=True)
+    email=db.Column(db.String(50))
+    name=db.Column(db.String(50))
+    gender=db.Column(db.String(50))
+    slot=db.Column(db.String(50))
+    disease=db.Column(db.String(50))
+    time=db.Column(db.String(50),nullable=False)
+    date=db.Column(db.String(50),nullable=False)
+    dept=db.Column(db.String(50))
+    number=db.Column(db.String(50))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # here we will pass endpoints and run the functions
 @app.route('/')
 def index():
@@ -52,21 +81,87 @@ def doctors():
     return render_template('doctor.html')
 
 
-@app.route('/patients')
+@app.route('/patients', methods=['POST', 'GET'])
+@login_required
 def patients():
+    if request.method == 'POST':
+        email=request.form.get('email')
+        name=request.form.get('name')
+        gender=request.form.get('gender')
+        slot=request.form.get('slot')
+        disease=request.form.get('disease')
+        time=request.form.get('time')
+        date=request.form.get('date')
+        dept=request.form.get('dept')
+        number=request.form.get('number')
+
+        query=db.engine.execute(f"INSERT INTO `patients` (`email`,`name`,`gender`,`slot`,`disease`,`time`,`date`,`dept`,`number`) VALUES ('{email}','{name}','{gender}','{slot}','{disease}','{time}','{date}','{dept}','{number}')")
+        flash("Booking Confirmed","info")
+
+
+
+
     return render_template('patient.html')
 
 
+
+# 50 min you tube video
 @app.route('/bookings')
+@login_required
 def bookings():
+    em=current_user.email
+    query=db.engine.execute(f"SELECT * FROM `patients` WHERE email='{em}'")
+    return render_template('booking.html',query=query)
 
-    if not user.is_authenticated:
-        return render_template('login.html')
-    else:
-        user = current_user.username
-        return render_template('booking.html', username=current_user.username)
 
-    return render_template('booking.html')
+
+@app.route("/edit/<string:pid>",methods=['POST','GET'])
+@login_required
+def edit(pid):
+    posts=Patients.query.filter_by(pid=pid).first()
+    if request.method=="POST":
+        email=request.form.get('email')
+        name=request.form.get('name')
+        gender=request.form.get('gender')
+        slot=request.form.get('slot')
+        disease=request.form.get('disease')
+        time=request.form.get('time')
+        date=request.form.get('date')
+        dept=request.form.get('dept')
+        number=request.form.get('number')
+        db.engine.execute(f"UPDATE `patients` SET `email` = '{email}', `name` = '{name}', `gender` = '{gender}', `slot` = '{slot}', `disease` = '{disease}', `time` = '{time}', `date` = '{date}', `dept` = '{dept}', `number` = '{number}' WHERE `patients`.`pid` = {pid}")
+        flash("Slot is Updates","success")
+        return redirect('/bookings')
+    
+    return render_template('edit.html',posts=posts)
+
+
+@app.route("/delete/<string:pid>",methods=['POST','GET'])
+@login_required
+def delete(pid):
+    db.engine.execute(f"DELETE FROM `patients` WHERE `patients`.`pid`={pid}")
+    flash("Slot Deleted Successful","danger")
+    return redirect('/bookings')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @app.route('/signup', methods=['POST', 'GET'])
@@ -77,13 +172,14 @@ def signup():
         password = request.form.get('password')
         user = User.query.filter_by(email=email).first()
         if user:
-            print("Email Already Exist", "warning")
+            flash("Email Already Exist", "warning")
             return render_template('/signup.html')
         encpassword = generate_password_hash(password)
 
         new_user = db.engine.execute(
             f"INSERT INTO `user` (`username`,`email`,`password`) VALUES ('{username}','{email}','{encpassword}')")
 
+        flash("Signup Succes Please Login", "success")
         return render_template('login.html')
 
     return render_template('signup.html')
@@ -97,9 +193,10 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user and check_password_hash(user.password, password):
             login_user(user)
-            return redirect(url_for('bookings'))
+            flash('You are now logged in', "primary")
+            return redirect(url_for('index'))
         else:
-            print("Invalid Credentials")
+            flash("Invalid Credentials", "danger")
             return render_template('login.html')
 
     return render_template('login.html')
@@ -109,6 +206,7 @@ def login():
 @login_required
 def logout():
     logout_user()
+    flash("You are now logged out", "success")
     return redirect(url_for('login'))
 
 
